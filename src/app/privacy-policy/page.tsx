aaf674d47172a8587e4947a1017dd2f5e1a88888
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2024-06-26 11:37:36 +0200

    Update Privacy Policy

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -8,7 +8,7 @@ export default function PrivacyPolicy() {
           <article className="prose max-w-none text-justify prose-headings:text-foreground prose-p:text-muted-foreground">
             <h1>Privacy Policy</h1>
 
-            <h2>Effective Date: 23/05/2024</h2>
+            <h2>Effective Date: 26/06/2024</h2>
 
             <p>
               Welcome to 42calculator! Your privacy is important to us. This
@@ -21,11 +21,13 @@ export default function PrivacyPolicy() {
 
             <h2>1. Information We Collect</h2>
 
-            <h3>&#x2022; Temporary Session Data</h3>
+            <h3>&#x2022; Temporary Data Storage</h3>
             <p>
-              We do not store any personal information from our users. Any data
-              retrieved from the 42 API is stored temporarily in the session and
-              is not retained after the session ends.
+              We do not store any personal information from our users
+              permanently. Any data retrieved from the 42 API, including the
+              user&apos;s 42 cursus and its projects, is stored temporarily in a
+              Redis instance for a duration of 24 hours. This data is fetched
+              when you log in to the site.
             </p>
 
             <h3>&#x2022; Cookies</h3>
@@ -40,16 +42,18 @@ export default function PrivacyPolicy() {
 
             <h3>&#x2022; Session Management</h3>
             <p>
-              The temporary data stored in the session is used to fetch relevant
-              information from the 42 API during your visit. This data is not
-              stored or retained after the session ends.
+              The temporary data stored in the Redis instance, including the
+              user&apos;s 42 cursus and its projects, is used to fetch relevant
+              information from the 42 API during your visit. This data is stored
+              in a Redis instance for up to 24 hours and is not retained beyond
+              this period.
             </p>
 
             <h2>3. Disclosure of Your Information</h2>
             <p>
               We do not share, sell, or otherwise disclose any information to
-              third parties. Since we do not store any user information, there
-              is no personal data to disclose.
+              third parties. Since we do not store any user information
+              permanently, there is no personal data to disclose.
             </p>
 
             <h2>4. Cookies and Tracking Technologies</h2>
@@ -72,7 +76,16 @@ export default function PrivacyPolicy() {
               interception or other type of misuse.
             </p>
 
-            <h2>6. Changes to This Privacy Policy</h2>
+            <h2>6. Data Deletion</h2>
+            <p>
+              Users have the ability to delete their information stored in our
+              system. Within the user management section, there is a dedicated
+              button that allows you to delete all your data, including your 42
+              cursus and its projects, from our system. If you wish to delete
+              your data, you can do so easily by using this feature.
+            </p>
+
+            <h2>7. Changes to This Privacy Policy</h2>
             <p>
               We may update this Privacy Policy from time to time in order to
               reflect changes to our practices or for other operational, legal,
