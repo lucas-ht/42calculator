eafa493f6987960477fb4e275aa89737e23953fc
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2024-07-04 19:46:36 +0200

    Add user deletion button

---
+++ src/components/header/user-menu/delete-user.tsx
@@ -0,0 +1,29 @@
+import { signOut } from '@/auth'
+import { Button } from '@/components/ui/button'
+import { kv } from '@vercel/kv'
+import { Session } from 'next-auth'
+
+export interface SignOutProps {
+  session: Session
+}
+
+export async function DeleteUser({ session }: SignOutProps) {
+  return (
+    <form
+      action={async () => {
+        'use server'
+        await kv.del(`cursus:${session.user.login}`)
+        await signOut({ redirectTo: '/' })
+      }}
+      className="size-full"
+    >
+      <Button
+        type="submit"
+        variant="ghost"
+        className="size-full justify-start text-destructive"
+      >
+        Delete my information
+      </Button>
+    </form>
+  )
+}
