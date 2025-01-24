1affcd5d0823e9cd4329b6c89273525868527cff
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2025-01-24 13:12:48 +0100

    feat: Merge delete user and sign out

--- src/components/header/user-menu/sign-out.tsx
+++ src/components/header/user-menu/sign-out.tsx
@@ -1,11 +1,14 @@
 import { signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { kv } from "@vercel/kv";
+import type { Session } from "next-auth";
 
-export function SignOut() {
+export function SignOut({ session }: { session: Session }) {
   return (
     <form
       action={async () => {
         "use server";
+        await kv.del(`cursus:${session.user.login}`);
         await signOut({ redirectTo: "/" });
       }}
       className="size-full"
