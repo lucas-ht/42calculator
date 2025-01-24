fb2bc0faf7e42726b1388099235438a0466a3a06
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2025-01-24 11:51:44 +0100

    feat: Add reset button in table action

--- src/app/calculator/(table)/table-actions.tsx
+++ src/app/calculator/(table)/table-actions.tsx
@@ -8,11 +8,14 @@ import {
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import type { Column as ColumnInstance } from "@tanstack/react-table";
 import { Ellipsis } from "lucide-react";
 import type { Row } from "@tanstack/react-table";
-import { ChevronsUpDownIcon } from "lucide-react";
+import { ChevronsUpDownIcon, Trash2 } from "lucide-react";
+import { useCalculatorStore } from "@/stores/use-calculator-store";
 
 export function TableAction<TData>({
   columns,
@@ -30,6 +33,21 @@ export function TableAction<TData>({
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
+        <DropdownMenuItem
+          className="group pr-2 pl-8"
+          onClick={() => {
+            const { resetProjects } = useCalculatorStore.getState();
+            resetProjects();
+          }}
+        >
+          <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+            <Trash2 className="size-4 transition-colors group-hover:stroke-destructive" />
+          </span>
+          <span className="transition-colors group-hover:text-destructive">
+            Reset projects
+          </span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
         {columns.map((column) => (
           <DropdownMenuCheckboxItem
             key={column.id}
